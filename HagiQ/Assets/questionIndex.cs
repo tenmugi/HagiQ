c52401e5f40329e8fb8d33ba19d577d5b9e0687a
tenmugi <89638729+tenmugi@users.noreply.github.com>
2021-11-02 18:00:32 +0900

    made script

---
+++ HagiQ/Assets/questionIndex.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class questionIndex : MonoBehaviour
+{
+    private bool firstPush = false;
+
+    public void PressValue3()
+    {
+        if (!firstPush)
+        {
+            SceneManager.LoadScene("quiz");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
